f47b137bbcad602225e454dffdcacb23c5a81c85
StefRad <63404251+StefRad@users.noreply.github.com>
2020-05-05 15:38:38 +0200

    prodotto tra quaternioni aggiunto

--- avanzato/biquaternion.py
+++ avanzato/biquaternion.py
@@ -16,7 +16,14 @@ class Biquaternion:
 		return rep
 
 	def scalar_prod(self, other):
-		return self.R*other.R - self.I*other.I - self.J*other.J - self.K*other.K
+		return self.R*other.R + self.I*other.I + self.J*other.J + self.K*other.K
+	
+	def prod(self, other):
+		r1 = self.R*other.R + self.I*other.I + self.J*other.J + self.K*other.K
+		i1 = 1j * ( self.R*other.I + self.I*other.R + 1j*self.J*other.K - 1j*self.K*other.J )
+		j1 = 1j * ( self.R*other.J + self.J*other.R - 1j*self.I*other.K + 1j*self.K*other.I )
+		k1 = 1j * ( self.R*other.K + self.K*other.R + 1j*self.I*other.J - 1j*self.J*other.I )
+		return Biquaternion(r1,i1,j1,k1)
             
 
 q = Biquaternion(1, 3+1j, 2j, 0)
